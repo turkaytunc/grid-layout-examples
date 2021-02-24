efd80c189b14b7a9dd65e319cb587a80c3cf9c5b
turkaytunc <turkaytunc00@gmail.com>
2021-02-24 12:00:55 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './app.scss';
 function App() {
   return (
     <div className="app">
-      <section className="grid-container"></section>
+      <section className="grid-container">Dev</section>
       <section className="grid-container"></section>
       <section className="grid-container"></section>
     </div>
