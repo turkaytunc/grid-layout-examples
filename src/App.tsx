e3fb929d23cb45a985f6324f8275fcacf90325b4
turkaytunc <turkaytunc00@gmail.com>
2021-02-24 13:18:08 +0300

    add: pancake layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './app.scss';
 function App() {
   return (
     <div className="app">
-      <h1> Holy Grail</h1>
+      <h1>Holy Grail</h1>
       <section className="grid-container-1">
         <header className="header visual">Header</header>
         <aside className="aside-1 visual">Aside-1</aside>
@@ -11,18 +11,17 @@ function App() {
         <aside className="aside-2 visual">Aside-2</aside>
         <footer className="footer visual">Footer</footer>
       </section>
-      <h1> Blog</h1>
+      <h1>Blog with Sticky Header</h1>
       <section className="grid-container-2">
         <header className="header visual">Header</header>
         <main className="main visual">Main Content</main>
         <aside className="ads visual">Ads</aside>
         <footer className="footer visual">Footer</footer>
       </section>
+      <h1>Pancake</h1>
       <section className="grid-container-3">
         <header className="header visual">Header</header>
-        <aside className="aside-1 visual">Aside-1</aside>
         <main className="main visual">Main Content</main>
-        <aside className="aside-2 visual">Aside-2</aside>
         <footer className="footer visual">Footer</footer>
       </section>
     </div>
